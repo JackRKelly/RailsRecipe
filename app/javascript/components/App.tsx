ee76a43252e9079276699e3b6483ba06cbeb4bf2
JackRKelly <kcjackkelly@gmail.com>
2020-11-17 12:08:58 -0600

    Added typescript react index and app

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
-  return <h1>App</h1>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- app/javascript/packs/Index.tsx
+++ app/javascript/packs/Index.tsx
@@ -1,7 +1,8 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import "./index.scss";
 import App from "../components/App";
+import { StrictMode } from "react";
 
 ReactDOM.render(
   <StrictMode>
