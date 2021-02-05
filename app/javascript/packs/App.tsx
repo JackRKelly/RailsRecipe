e5bb85e999d4e8778f0ed361881c20e1f1394cad
Jack Kelly <kcjackkelly@gmail.com>
2021-02-05 11:29:25 -0600

    Updated navigation structure

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GlobalStyle from "../components/GlobalStyle";
+import CreateRecipe from "../pages/CreateRecipe";
 import Home from "../pages/Home";
 import Recipe from "../pages/Recipe";
 import RecipeView from "../pages/RecipeView";
@@ -13,10 +14,21 @@ const App: React.FC = () => {
       <nav id="navigation">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <ul>
+              <li>
+                <Link to="/">RailsRecipe</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <Link to="/recipe">Recipe</Link>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/recipe">Recipes</Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
@@ -27,6 +39,9 @@ const App: React.FC = () => {
         </Route>
         <Route exact path="/recipe">
           <Recipe />
+        </Route>
+        <Route exact path="/recipe/create">
+          <CreateRecipe />
         </Route>
         <Route exact path="/recipe/view/:id">
           <RecipeView />
