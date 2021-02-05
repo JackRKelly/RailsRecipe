f5b571adf24e7986f21d5279aaff323b48bfe765
Jack Kelly <kcjackkelly@gmail.com>
2021-02-05 14:26:08 -0600

    Added image validation to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
+  validates :image, presence: true
   validates :ingredients, presence: true
   validates :instructions, presence: true
 end
